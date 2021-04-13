c3a1c50a45a78a24662a7f96e3c8ed0ad4175dcf
gdis5251 <841784596@qq.com>
2021-04-13 18:09:19 +0800

    basic scala

--- src/main/scala/com/bytedance/scala/basic_scala/BasicScala.scala
+++ src/main/scala/com/bytedance/scala/basic_scala/BasicScala.scala
@@ -7,10 +7,24 @@ object BasicScala {
    */
   def mul(x: Int)(y: Int): Int = x * y
 
+  /***
+   * 可变长参数
+   */
+  def capitalizeAll(args: String*): Unit = {
+    // capitalize: 将首字母变成大写
+    args.foreach(x => {
+      println(x.capitalize)
+    })
+  }
+
   def main(args: Array[String]): Unit = {
     // 柯里化函数
     val result = mul(2)_
     println(result(3))
+    // end
+
+    // 可变长参数
+    capitalizeAll("aaa", "bbb")
     // end
 
 
