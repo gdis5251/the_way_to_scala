09e7823feee74bf24f7b4859211950924bfbbe9a
gdis5251 <841784596@qq.com>
2021-04-13 23:44:27 +0800

    求二进制位的值

--- src/main/java/com/bytedance/java/basic/BasicPractice.java
+++ src/main/java/com/bytedance/java/basic/BasicPractice.java
@@ -16,8 +16,29 @@ public class BasicPractice {
         System.out.println(count);
     }
 
+    private void test() {
+        System.out.println("test");
+    }
+
+    public static int getBiteValue(int num, int offset) {
+        return (num >> offset) & 1;
+    }
 
     public static void main(String[] args) {
-        getOneBite();
+//        getOneBite();
+
+        Scanner scanner = new Scanner(System.in);
+        int input = scanner.nextInt();
+
+        // 获取一个二进制数中奇数位和偶数位
+        for (int i = 31; i > 0; i -= 2) {
+            System.out.printf("%d ", getBiteValue(input, i));
+        }
+        System.out.println();
+
+        for (int i = 30; i >= 0; i -= 2) {
+            System.out.printf("%d ", getBiteValue(input, i));
+        }
+        System.out.println();
     }
 }
